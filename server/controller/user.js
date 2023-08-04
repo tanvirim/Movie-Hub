304eec37449a1683e5bc136639c49dce138eb836
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-08-04 06:37:07 +0600

    added boilerplate for loaderSlice and userSlice and fixed current-user router controller

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,11 +1,21 @@
 const User = require('../models/user')
 const currentUserController = async (req , res , next) => {
-    const user = await User.findById(req.user._id).select("-password")
-    res.send({
-      success: true,
-      message: "current user fetched successfully",
-      data: user ,
-    })
+try {
+  const user = await User.findById(req.user._id).select("-password")
+  res.send({
+    success: true,
+    message: "current user fetched successfully",
+    data: user ,
+  })
+} catch (error) {
+  res.status(500).send({
+    success: false,
+    message: "An error occurred while fetching current user",
+    error: error.message, // This will send the error message to the client
+  });
+   
+  
+}
 }
 
 module.exports = currentUserController
