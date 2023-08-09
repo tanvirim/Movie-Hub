0973b6b20f469f453e28128fea5f2af67d1d2409
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-08-09 20:23:22 +0600

    added add movie button

---
+++ client/src/pages/Admin/TheatresList.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react"
+
+
+const TheatresList = () => {
+  return (
+<Box>
+  theaters
+</Box>
+  )
+}
+
+export default TheatresList
--- client/src/pages/Admin/index.jsx
+++ client/src/pages/Admin/index.jsx
@@ -1,14 +1,35 @@
 
+import { Box ,Tabs, TabList , Tab, TabPanels ,TabPanel} from "@chakra-ui/react";
 import PageTitle from "../../components/PageTitle";
+import MoviesList from "./MoviesList";
+import TheatresList from "./TheatresList";
 
 
 function Admin() {
   return (
-    <div>
-      <PageTitle title="Admin" />
+<>
+<PageTitle title="Admin" />
+<Box>
+<Tabs isLazy>
+  <TabList>
+    <Tab>Movies</Tab>
+    <Tab>Theaters</Tab>
+  </TabList>
+  <TabPanels>
+    {/* initially mounted */}
+    <TabPanel>
+      <MoviesList/>
+    </TabPanel>
+    {/* initially not mounted */}
+    <TabPanel>
+      <TheatresList/>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
 
-    </div>
-  );
+</Box>
+  </>
+  )
 }
 
 export default Admin;
